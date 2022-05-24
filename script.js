5cbbc60d7af6b8c028b5ef609d0d5cc4d6afa62e
Bartłomiej Łysik <100691680+Bartek2134@users.noreply.github.com>
2022-05-24 10:54:52 +0000

    6.1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-(function () {
-  
+(function() {
+  const ex1 = document.getElementById('ex1_button')
+  const ex1c = document.getElementById('ex1_content')
+  ex1.addEventListener("click", function() {
+    var data = " ";
+    for (var i = 0; i <= 9; i++) {
+      if (i != 0)
+        data += ",";
+      data += i.toString();
+    }
+    ex1c.innerHTML = data;
+  })
 })();
